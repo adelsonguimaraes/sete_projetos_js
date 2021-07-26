aa269183c3e16ef65f2c181d9341d869589b6d73
Adelson Guimarães Monteiro <adelsonguimaraes@gmail.com>
2021-07-25 23:33:22 -0400

    subindo o projeto para o git

---
+++ bateria/script.js
@@ -0,0 +1,42 @@
+document.body.addEventListener('keyup', (e) => {
+    playSound(event.code.toLowerCase());
+});
+
+document.querySelector('.composer button').addEventListener('click', _=> {
+    let song = document.querySelector('#input').value;
+
+    if (song !== '') {
+        let songArray = song.split('');
+        playComposition(songArray);
+    }
+});
+
+function playSound(sound) {
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
+    if (audioElement) {
+        audioElement.currentTime = 0;
+        audioElement.play();
+    }
+
+    if (keyElement) {
+        keyElement.classList.add('active');
+
+        setTimeout(_=>{
+            keyElement.classList.remove('active');
+        }, 300);
+    }
+}
+
+function playComposition (songArray) {
+    let wait = 0;
+
+    for (let songItem of songArray) {
+        setTimeout(_=>{
+            playSound(`key${songItem}`);
+        }, wait);
+        
+        wait += 250;
+    }
+}
